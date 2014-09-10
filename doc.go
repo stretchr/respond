437c2921eac633ac28dcaed9905a126f57b29b0f
Mat Ryer <mat@stretchr.com>
2014-09-10 00:27:07 -0600

    more examples

--- doc.go
+++ doc.go
@@ -1,6 +1,10 @@
 // Package respond provides a simple and consistent way of writing
 // data API responses when building web services.
 //
+//     // write some data
 //     respond.With{Data:obj}.To(w,r)
+//
+//     // with specific status
+//     respond.With{Data:obj, Status:http.StatusCreated}.To(w,r)
 //
 package respond
